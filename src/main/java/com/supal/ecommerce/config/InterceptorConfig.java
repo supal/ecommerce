6a5f140180ec509970088ca34c960a4f1780a86f
ariful <supal.iut03@gmail.com>
2024-11-26 21:14:10 +0600

    Added logback and gray log

---
+++ src/main/java/com/supal/ecommerce/config/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package com.supal.ecommerce.config;
+
+import com.supal.ecommerce.interceptor.LogInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig implements WebMvcConfigurer {
+    private final LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor).addPathPatterns("/**");
+    }
+}
--- src/main/java/com/supal/ecommerce/service/hello/HelloServiceImpl.java
+++ src/main/java/com/supal/ecommerce/service/hello/HelloServiceImpl.java
@@ -1,11 +1,14 @@
 package com.supal.ecommerce.service.hello;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class HelloServiceImpl implements HelloService {
     @Override
     public String hello() {
+        log.info("hello");
         return "hello";
     }
 }
